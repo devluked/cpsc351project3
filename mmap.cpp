6b58458e69611290f03380e6f2266063f6e42beb
devluked <devlukeduggan@gmail.com>
2020-12-01 22:56:42 -0800

    Finished?

--- mmap.cpp
+++ mmap.cpp
@@ -11,6 +11,8 @@
 using namespace std;
 using namespace std::chrono;
 
+#define OUTPUT_MODE 0700
+
 int main(int argc, char** argv)
 {
 	/* Make sure the command line is correct */
@@ -30,53 +32,73 @@ int main(int argc, char** argv)
 		cout<<"\n"<<"input file cannot be opened"<<"\n";
 		exit(1);
 	}
+	
 
-	struct stat stats;
-	if (stat(argv[1], &stats)==0)
-      cout<<endl<<"file size "<<stats.st_size;
-  else
-	    cout<<"Unable to get file properties.\n";
-
+	// stats.st_size is a variable containing the size of inFile.txt
+	struct stat instats;
+	if (stat(argv[1], &instats)==0)
+      		cout<<endl<<"inFile size "<<instats.st_size;
+  	else
+	       cout<<"Unable to get file properties.\n";
+	       
+	       
 	/* Get the page size  */
 	int pagesize = getpagesize();
 	cout<<endl<<"page size is " <<pagesize<<"\n";
 
+
+	/******************Creation of outFile.txt******************/
+	int out_fd = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, OUTPUT_MODE);
+
+
+	if (out_fd < 0) {
+		cout << "\n" << "output file cannot be created" << "\n";
+		exit(1);        // if it cannot be created, exit
+	}
+	
 	/* map the file into memory */
-	char* data = (char*)mmap(NULL, pagesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd,
-	0);
-
-
-	/* Did the mapping succeed ? */
-	if(!data)
+	char* in_data = (char*)mmap(NULL, instats.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	// Checks if mapping succeeded 
+	if(!in_data)
 	{
 		cout<<"\n"<<"mapping did not succeed"<<"\n";
 		exit(1);
 	}
+	
+	/* file must be same size so memory can be allocated */
+	ftruncate(out_fd, instats.st_size);
+	/* map the file into memory */
+	char* out_data = (char*)mmap(NULL, instats.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, out_fd, 0);
+	if(!out_data)
+	{
+		cout<<"\n"<<"mapping did not succeed"<<"\n";
+		exit(1);
+	}
+	memcpy(out_data, in_data, instats.st_size);
 
 
-		/* Print the whole file character-by-character */
+	/*//Print the whole file character-by-character 
 	for(int fIndex = 0; fIndex < pagesize; ++fIndex)
 	{
 
-		cout<<data[fIndex];
-		/*if((fIndex%1000)==1)
-		    cout<<endl;*/
-
+		cout<<in_data[fIndex];
+		if((fIndex%1000)==1)
+		    cout<<endl;
 	}
-  cout<<endl;
-	/* Write a string to the mapped region */
-	/*memcpy(data, "Hello world, this is a test\n", sizeof("Hello world, this is a test"));*/
-
+  	cout<<endl;
+  	*/
+  	
 	/* Unmap the shared memory region */
-	munmap(data, pagesize);
+	munmap(in_data, instats.st_size);
+	munmap(out_data, instats.st_size);
 
 	/* Close the file */
 	close(fd);
-  auto stop = high_resolution_clock::now();
-
+	close(out_fd);
+	
+  	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<microseconds>(stop - start);
 
-    cout << "Time taken by function: "
-         << duration.count() << " microseconds" << endl;
+    	cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
 	return 0;
 }
--- readme.txt
+++ readme.txt
@@ -3,3 +3,14 @@ Group members:
 Luke Duggan luke.duggan@csu.fullerton.edu
 Benedict Barnum bmbarnum@csu.fullerton.edu
 Iftekharul Islam iislam99@csu.fullerton.edu
+
+
+Timing: 
+
+Time taken by function: 6211547 microseconds
+Time taken by function: 8555296 microseconds
+Time taken by function: 8112048 microseconds
+Time taken by function: 2684743 microseconds
+Time taken by function: 8669087 microseconds
+
+Avg time: 6846544 microseconds
